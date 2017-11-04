2c17a3735e68cdad31eac6201e0c738fb4f3035c
ntnawsm <cnitin52@gmail.com>
2017-11-04 19:05:49 +0530

    bla

--- app/src/main/java/com/example/jiteshnarula/mess/MainActivity.java
+++ app/src/main/java/com/example/jiteshnarula/mess/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.jiteshnarula.mess;
 
+import android.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.annotation.IdRes;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
@@ -15,19 +19,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ViewPager vp = findViewById(R.id.viewpager);
+        vp.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
 
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
 //                messageView.setText(TabMessage.get(tabId, false));
+                switch(tabId)
+                {
+                    case R.id.tab_recents:
+                        vp.setCurrentItem(0);
+                        break;
+
+                    case R.id.tab_friends:
+                        vp.setCurrentItem(1);
+                        break;
+
+                    case R.id.tab_favorites:
+                        vp.setCurrentItem(2);
+                        break;
+
+                    case R.id.tab_nearby:
+                        vp.setCurrentItem(3);
+                        break;
+
+                    case R.id.tab_food:
+                        vp.setCurrentItem(4);
+                        break;
+
+
+
+                }
             }
         });
 
         bottomBar.setOnTabReselectListener(new OnTabReselectListener() {
             @Override
             public void onTabReSelected(@IdRes int tabId) {
+                Log.i("ntn","----->" + tabId);
 //                Toast.makeText(getApplicationContext(), TabMessage.get(tabId, true), Toast.LENGTH_LONG).show();
+
+
             }
         });
     }
--- app/src/main/java/com/example/jiteshnarula/mess/Profile.java
+++ app/src/main/java/com/example/jiteshnarula/mess/Profile.java
@@ -1,17 +1,28 @@
 package com.example.jiteshnarula.mess;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Profile extends Fragment {
+    public static final int REQUEST_CAPTURE = 1, SELECT_FILE = 0;
+
+    ImageView dp,edit;
 
 
     public Profile() {
@@ -23,7 +34,63 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v =  inflater.inflate(R.layout.fragment_profile, container, false);
+        dp = (ImageView)v.findViewById(R.id.dp);
+        edit = (ImageView)v.findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!hasCamera())
+
+                {
+
+                    Toast.makeText(getActivity(), "No Camera Found", Toast.LENGTH_SHORT).show();
+                } else {
+                    selectImage();
+
+                }
+            }
+        });
+        return v;
+    }
+    public void selectImage() {
+        final CharSequence[] item = {"Camera", "Gallery", "Cancel"};
+        AlertDialog.Builder ab = new AlertDialog.Builder(getActivity());
+        ab.setTitle("Add Image");
+        ab.setItems(item, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                if (item[i].equals("Camera")) {
+                    Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cam, REQUEST_CAPTURE);
+
+
+                } else if (item[i].equals("Gallery")) {
+                    Intent gal = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    gal.setType("image/*");
+                    gal.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(gal.createChooser(gal, "Select File"), SELECT_FILE);
+
+                }
+                if (item[i].equals("Cancel")) {
+                    dialogInterface.dismiss();
+                }
+
+            }
+        });
+
+        AlertDialog a = ab.create();
+        a.show();
+    }
+    public boolean hasCamera() {
+        PackageManager getPackageManager=getActivity().getPackageManager();
+        return getPackageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
